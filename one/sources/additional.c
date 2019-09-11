2372f5b070faf5e478531e6cb7b567093db1c0d4
Ruben Satterfield <rsatterf@oa-f1.21-school.ru>
2019-09-11 21:55:42 +0300

    norm

--- one/sources/additional.c
+++ one/sources/additional.c
@@ -6,97 +6,105 @@
 /*   By: rsatterf <rsatterf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/24 13:45:02 by nbethany          #+#    #+#             */
-/*   Updated: 2019/09/11 18:37:20 by rsatterf         ###   ########.fr       */
+/*   Updated: 2019/09/11 21:21:07 by rsatterf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
-void        load_textures(t_wolf_3d *wolf)
+void	load_textures(t_wolf_3d *wolf)
 {
-    int tex_width = 64;
-    int tex_height = 64;
-    int a = 512;
-    int b = 512;
-    int pistol_res_width = 228;
-    int pistol_res_height = 317;
+	int tex_width = 64;
+	int tex_height = 64;
+	int a = 512;
+	int b = 512;
+	int pistol_res_width = 228;
+	int pistol_res_height = 317;
 
-    wolf->tex[0].image = mlx_xpm_file_to_image(wolf->mlx,"textures/stone.xpm", &tex_width, &tex_height);
-    wolf->tex[0].ptr = mlx_get_data_addr(wolf->tex[0].image, &wolf->tex[0].bpp, &wolf->tex[0].line_s, &wolf->tex[0].endian);
-    wolf->tex[0].bpp /= 8;
-    wolf->tex[1].image = mlx_xpm_file_to_image(wolf->mlx,"textures/sand.xpm", &tex_width, &tex_height);
-    wolf->tex[1].ptr = mlx_get_data_addr(wolf->tex[1].image, &wolf->tex[1].bpp, &wolf->tex[1].line_s, &wolf->tex[1].endian);
-    wolf->tex[1].bpp /= 8;
-    wolf->tex[2].image = mlx_xpm_file_to_image(wolf->mlx,"textures/wood.xpm", &tex_width, &tex_height);
-    wolf->tex[2].ptr = mlx_get_data_addr(wolf->tex[2].image, &wolf->tex[2].bpp, &wolf->tex[2].line_s, &wolf->tex[2].endian);
-    wolf->tex[2].bpp /= 8;
-    wolf->tex[3].image = mlx_xpm_file_to_image(wolf->mlx,"textures/mossy.xpm", &tex_width, &tex_height);
-    wolf->tex[3].ptr = mlx_get_data_addr(wolf->tex[3].image, &wolf->tex[3].bpp, &wolf->tex[3].line_s, &wolf->tex[3].endian);
-    wolf->tex[3].bpp /= 8;
-    wolf->tex[4].image = mlx_xpm_file_to_image(wolf->mlx,"textures/sky.xpm", &a, &b);
-    wolf->tex[4].ptr = mlx_get_data_addr(wolf->tex[4].image, &wolf->tex[4].bpp, &wolf->tex[4].line_s, &wolf->tex[4].endian);
-    wolf->tex[4].bpp /= 8;
-    wolf->pistol[0].image = mlx_xpm_file_to_image(wolf->mlx,"textures/gun_1.xpm", &pistol_res_width, &pistol_res_height);
-    wolf->pistol[1].image = mlx_xpm_file_to_image(wolf->mlx,"textures/gun_2.xpm", &pistol_res_width, &pistol_res_height);
-    wolf->pistol[2].image = mlx_xpm_file_to_image(wolf->mlx,"textures/gun_3.xpm", &pistol_res_width, &pistol_res_height);
-    wolf->pistol[3].image = mlx_xpm_file_to_image(wolf->mlx,"textures/gun_4.xpm", &pistol_res_width, &pistol_res_height);
+	wolf->tex[0].image = mlx_xpm_file_to_image(wolf->mlx,"textures/stone.xpm", &tex_width, &tex_height);
+	wolf->tex[0].ptr = mlx_get_data_addr(wolf->tex[0].image, &wolf->tex[0].bpp, &wolf->tex[0].line_s, &wolf->tex[0].endian);
+	wolf->tex[0].bpp /= 8;
+	wolf->tex[1].image = mlx_xpm_file_to_image(wolf->mlx,"textures/sand.xpm", &tex_width, &tex_height);
+	wolf->tex[1].ptr = mlx_get_data_addr(wolf->tex[1].image, &wolf->tex[1].bpp, &wolf->tex[1].line_s, &wolf->tex[1].endian);
+	wolf->tex[1].bpp /= 8;
+	wolf->tex[2].image = mlx_xpm_file_to_image(wolf->mlx,"textures/wood.xpm", &tex_width, &tex_height);
+	wolf->tex[2].ptr = mlx_get_data_addr(wolf->tex[2].image, &wolf->tex[2].bpp, &wolf->tex[2].line_s, &wolf->tex[2].endian);
+	wolf->tex[2].bpp /= 8;
+	wolf->tex[3].image = mlx_xpm_file_to_image(wolf->mlx,"textures/mossy.xpm", &tex_width, &tex_height);
+	wolf->tex[3].ptr = mlx_get_data_addr(wolf->tex[3].image, &wolf->tex[3].bpp, &wolf->tex[3].line_s, &wolf->tex[3].endian);
+	wolf->tex[3].bpp /= 8;
+	wolf->tex[4].image = mlx_xpm_file_to_image(wolf->mlx,"textures/sky.xpm", &a, &b);
+	wolf->tex[4].ptr = mlx_get_data_addr(wolf->tex[4].image, &wolf->tex[4].bpp, &wolf->tex[4].line_s, &wolf->tex[4].endian);
+	wolf->tex[4].bpp /= 8;
+	wolf->pistol[0].image = mlx_xpm_file_to_image(wolf->mlx,"textures/gun_1.xpm", &pistol_res_width, &pistol_res_height);
+	wolf->pistol[1].image = mlx_xpm_file_to_image(wolf->mlx,"textures/gun_2.xpm", &pistol_res_width, &pistol_res_height);
+	wolf->pistol[2].image = mlx_xpm_file_to_image(wolf->mlx,"textures/gun_3.xpm", &pistol_res_width, &pistol_res_height);
+	wolf->pistol[3].image = mlx_xpm_file_to_image(wolf->mlx,"textures/gun_4.xpm", &pistol_res_width, &pistol_res_height);
 }
 
 void		img_pixel_put_one(t_image *img, int x, int y, int color)
 {
-    if (x>= 0 && x < WIDTH && y >= 0 && y < HEIGHT)
-        *(int *) (img->ptr + (int) ((WIDTH * y + x) * img->bpp)) = color;
+	if (x>= 0 && x < WIDTH && y >= 0 && y < HEIGHT)
+		*(int *)(img->ptr + (int)((WIDTH * y + x) * img->bpp)) = color;
 }
 
-void	draw_sky(t_image *img,t_wolf_3d *wolf)
+void		draw_sky(t_image *img, t_wolf_3d *wolf)
 {
-    wolf->tex_x = 0;
-    while (wolf->tex_x < WIDTH)
-    {
-        wolf->tex_y = 0;
-        while (wolf->tex_y < HEIGHT / 2)
-        {
-            ft_memcpy(img->ptr + 4 * WIDTH * wolf->tex_y + wolf->tex_x * 4, &wolf->tex[4].ptr[wolf->tex_y % 512 * wolf->tex[4].line_s + wolf->tex_x % 512 * wolf->tex[4].bpp], sizeof(int));
-            wolf->tex_y++;
-        }
-        wolf->tex_x++;
-    }
+	wolf->tex_x = 0;
+	while (wolf->tex_x < WIDTH)
+	{
+		wolf->tex_y = 0;
+		while (wolf->tex_y < HEIGHT / 2)
+		{
+			ft_memcpy(img->ptr + 4 * WIDTH * wolf->tex_y + wolf->tex_x * 4,
+				&wolf->tex[4].ptr[wolf->tex_y % 512 * wolf->tex[4].line_s +
+					wolf->tex_x % 512 * wolf->tex[4].bpp], sizeof(int));
+			wolf->tex_y++;
+		}
+		wolf->tex_x++;
+	}
 }
 
-void pistol_animation(t_wolf_3d *wolf)
+void	pistol_animation(t_wolf_3d *wolf)
 {
-    if (wolf->shot_frames >= 11)
-        mlx_put_image_to_window(wolf->mlx, wolf->window, wolf->pistol[1].image, (WIDTH - 100) / 2, HEIGHT - 300);
-    else if (wolf->shot_frames >= 6 && wolf->shot_frames < 11)
-        mlx_put_image_to_window(wolf->mlx, wolf->window, wolf->pistol[2].image, (WIDTH - 100) / 2, HEIGHT - 300);
-    else if (wolf->shot_frames >= 3 && wolf->shot_frames < 6)
-        mlx_put_image_to_window(wolf->mlx, wolf->window, wolf->pistol[3].image, (WIDTH - 100) / 2, HEIGHT - 300);
-    else if (wolf->shot_frames == 2)
-        mlx_put_image_to_window(wolf->mlx, wolf->window, wolf->pistol[3].image, (WIDTH - 100) / 2, HEIGHT - 300);
-    wolf->shot_frames--;
-    if (wolf->shot_frames == 1)
-    {
-        wolf->shot_frames = 16;
-        wolf->shot_flag = 0;
-    }
+	if (wolf->shot_frames >= 11)
+		mlx_put_image_to_window(wolf->mlx, wolf->window,
+			wolf->pistol[1].image, (WIDTH - 100) / 2, HEIGHT - 300);
+	else if (wolf->shot_frames >= 6 && wolf->shot_frames < 11)
+		mlx_put_image_to_window(wolf->mlx, wolf->window,
+			wolf->pistol[2].image, (WIDTH - 100) / 2, HEIGHT - 300);
+	else if (wolf->shot_frames >= 3 && wolf->shot_frames < 6)
+		mlx_put_image_to_window(wolf->mlx, wolf->window,
+			wolf->pistol[3].image, (WIDTH - 100) / 2, HEIGHT - 300);
+	else if (wolf->shot_frames == 2)
+		mlx_put_image_to_window(wolf->mlx, wolf->window,
+			wolf->pistol[3].image, (WIDTH - 100) / 2, HEIGHT - 300);
+	wolf->shot_frames--;
+	if (wolf->shot_frames == 1)
+	{
+		wolf->shot_frames = 16;
+		wolf->shot_flag = 0;
+	}
 }
 
 void	animate_pistol(t_wolf_3d *wolf)
 {
-    if (wolf->shot_flag == 0)
-        mlx_put_image_to_window(wolf->mlx, wolf->window, wolf->pistol[0].image, (WIDTH - 100) / 2, HEIGHT - 300);
-    else if (wolf->shot_flag == 1)
-        {
-        if (wolf->shot_frames == 16)
-            OS_VER == 0 ? system("afplay -v 0.65 audio/gun_shot.wav &") : system("paplay --volume 35000 audio/gun_shot.wav");
-        pistol_animation(wolf);
-    }
+	if (wolf->shot_flag == 0)
+		mlx_put_image_to_window(wolf->mlx, wolf->window,
+			wolf->pistol[0].image, (WIDTH - 100) / 2, HEIGHT - 300);
+	else if (wolf->shot_flag == 1)
+	{
+		if (wolf->shot_frames == 16)
+			OS_VER == 0 ? system("afplay -v 0.65 audio/gun_shot.wav &") :
+				system("paplay --volume 35000 audio/gun_shot.wav");
+		pistol_animation(wolf);
+	}
 }
 
-void draw_floor(int x, int side, t_wolf_3d *wolf)
+void	draw_floor(int x, int side, t_wolf_3d *wolf)
 {
-    double floor_x_wall;
-    double floor_y_wall;
+	double floor_x_wall;
+	double floor_y_wall;
     double current_dist;
     int y;
 
@@ -149,75 +157,64 @@ void draw_floor(int x, int side, t_wolf_3d *wolf)
 
 }
 
-void init_wolf(t_wolf_3d *wolf)
+void	init_wolf(t_wolf_3d *wolf)
 {
-    wolf->dir_x = -1;
-    wolf->dir_y = 0;
-    wolf->plane_x = 0;
-    wolf->plane_y = 0.66;
-    wolf->move_speed = 0.059;
-    wolf->rotate_speed =  M_PI / 72;
-    wolf->color = 0;
-    wolf->flag = 0;
-    wolf->shot_flag = 0;
-    wolf->shot_frames = 16;
-    wolf->move.forward = 0;
-    wolf->move.back = 0;
-    wolf->move.left = 0;
-    wolf->move.right = 0;
-    load_textures(wolf);
-    // FOV = 2 * arctan(planeY / 1.0) - in degrees
+	wolf->dir_x = -1;
+	wolf->dir_y = 0;
+	wolf->plane_x = 0;
+	wolf->plane_y = 0.66;
+	wolf->move_speed = 0.059;
+	wolf->rotate_speed = M_PI / 72;
+	wolf->color = 0;
+	wolf->flag = 0;
+	wolf->shot_flag = 0;
+	wolf->shot_frames = 16;
+	wolf->move.forward = 0;
+	wolf->move.back = 0;
+	wolf->move.left = 0;
+	wolf->move.right = 0;
+	load_textures(wolf);
 }
 
-void draw_walls(int x, int side, t_wolf_3d *wolf)
+void	draw_walls(int x, int side, t_wolf_3d *wolf)
 {
-    wolf->tex_num = (wolf->map[wolf->map_x][wolf->map_y] - 1) % 4;
-    if (side == 0)
-        wolf->wall_x = wolf->pos_y + wolf->wall_dist * wolf->ray_dir_y;
-    else
-        wolf->wall_x = wolf->pos_x + wolf->wall_dist * wolf->ray_dir_x;
-    wolf->wall_x -= floor(wolf->wall_x);
-    wolf->tex_x = (int) (wolf->wall_x * (double) TEX_WIDTH);
+	int d;
 
-    if ((side == 0 && wolf->ray_dir_x > 0) || (side == 1 && wolf->ray_dir_y < 0))
-        wolf->tex_x = TEX_WIDTH - wolf->tex_x - 1;
-
-    while (wolf->draw_start < wolf->draw_end)
-    {
-        int d = wolf->draw_start - HEIGHT * 0.5f + wolf->line_height * 0.5f;
-        wolf->tex_y = ((d * TEX_HEIGHT) / wolf->line_height);
-		ft_memcpy(&wolf->color, &wolf->tex[wolf->tex_num].ptr[wolf->tex_y % 64 * wolf->tex[wolf->tex_num].line_s + wolf->tex_x % 64 * wolf->tex[wolf->tex_num].bpp], sizeof(int));
+	wolf->tex_num = (wolf->map[wolf->map_x][wolf->map_y] - 1) % 4;
+	if (side == 0)
+		wolf->wall_x = wolf->pos_y + wolf->wall_dist * wolf->ray_dir_y;
+	else
+		wolf->wall_x = wolf->pos_x + wolf->wall_dist * wolf->ray_dir_x;
+	wolf->wall_x -= floor(wolf->wall_x);
+	wolf->tex_x = (int)(wolf->wall_x * (double)TEX_WIDTH);
+	if ((side == 0 && wolf->ray_dir_x > 0) ||
+		(side == 1 && wolf->ray_dir_y < 0))
+		wolf->tex_x = TEX_WIDTH - wolf->tex_x - 1;
+	while (wolf->draw_start < wolf->draw_end)
+	{
+		d = wolf->draw_start - HEIGHT * 0.5f + wolf->line_height * 0.5f;
+		wolf->tex_y = ((d * TEX_HEIGHT) / wolf->line_height);
+		ft_memcpy(&wolf->color, &wolf->tex[wolf->tex_num].ptr[wolf->tex_y %
+			64 * wolf->tex[wolf->tex_num].line_s + wolf->tex_x %
+				64 * wolf->tex[wolf->tex_num].bpp], sizeof(int));
 		if (side == 1)
 			wolf->color = wolf->color >> 1 & 0x7F7F7F;
-		img_pixel_put_one(&wolf->image, x, wolf->draw_start,wolf->color);
-        wolf->draw_start++;
-    }
+		img_pixel_put_one(&wolf->image, x, wolf->draw_start, wolf->color);
+		wolf->draw_start++;
+	}
 }
 
-/*void debug(t_wolf_3d *wolf)
+int		loop(t_wolf_3d *wolf)
 {
-    printf("pos_x - %f, pos_y - %f, wall_dist - %f, wall_x - %f, tex_num - %d, map_x - %d, map_y - %d, map - %d \n",wolf->pos_x, wolf->pos_y, wolf->wall_dist, wolf->wall_x,
-            wolf->tex_num, wolf->map_x, wolf->map_y, wolf->map[wolf->map_x][wolf->map_y]);
-}
-*/
-void	image_clear(t_image *im)
-{
-	ft_bzero(im->ptr, WIDTH * HEIGHT * im->bpp);
-}
-
-int      loop(t_wolf_3d *wolf)
-{
-	image_clear(&wolf->image);
-    movement(wolf);
-    draw_sky(&wolf->image, wolf);
-    ray_caster(wolf);
-    //debug(wolf);
-    mlx_put_image_to_window(wolf->mlx, wolf->window, wolf->image.image, 0, 0);
-    animate_pistol(wolf);
-    return (0);
+	movement(wolf);
+	draw_sky(&wolf->image, wolf);
+	ray_caster(wolf);
+	mlx_put_image_to_window(wolf->mlx, wolf->window, wolf->image.image, 0, 0);
+	animate_pistol(wolf);
+	return (0);
 }
 
-void ray_caster(t_wolf_3d *wolf)
+void	ray_caster(t_wolf_3d *wolf)
 {
     int x;
     //direction of the ray (1 or -1 for both step-x and step_ y)
